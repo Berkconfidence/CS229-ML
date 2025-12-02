7723389525c5a608e7471ced5fedb6b52e92cbf3
Berk Güven <berkconfidence@gmail.com>
2025-12-02 23:09:56 +0300

    week2 optional labs finished

---
+++ C1/W2/1_Feature_scaling.py
@@ -0,0 +1,68 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from lab_utils_multi import  load_house_data, run_gradient_descent 
+from lab_utils_multi import  norm_plot, plt_equal_scale, plot_cost_i_w
+from lab_utils_common import dlc
+np.set_printoptions(precision=2)
+plt.style.use('./deeplearning.mplstyle')
+
+# load the dataset
+X_train, y_train = load_house_data()
+X_features = ['size(sqft)','bedrooms','floors','age']
+
+
+fig,ax=plt.subplots(1, 4, figsize=(12, 6), sharey=True)
+for i in range(len(ax)):
+    ax[i].scatter(X_train[:,i],y_train)
+    ax[i].set_xlabel(X_features[i])
+ax[0].set_ylabel("Price (1000's)")
+plt.show()
+
+def zscore_normalize_features(X):
+    """
+    computes  X, zcore normalized by column
+    
+    Args:
+      X (ndarray (m,n))     : input data, m examples, n features
+      
+    Returns:
+      X_norm (ndarray (m,n)): input normalized by column
+      mu (ndarray (n,))     : mean of each feature
+      sigma (ndarray (n,))  : standard deviation of each feature
+    """
+    # find the mean of each column/feature
+    mu     = np.mean(X, axis=0)                 # mu will have shape (n,)
+    # find the standard deviation of each column/feature
+    sigma  = np.std(X, axis=0)                  # sigma will have shape (n,)
+    # element-wise, subtract mu for that column from each example, divide by std for that column
+    X_norm = (X - mu) / sigma      
+
+    return (X_norm, mu, sigma)
+
+
+X_norm, X_mu, X_sigma = zscore_normalize_features(X_train)
+print(f"X_mu = {X_mu}, \nX_sigma = {X_sigma}")
+print(f"Peak to Peak range by column in Raw        X:{np.ptp(X_train,axis=0)}")   
+print(f"Peak to Peak range by column in Normalized X:{np.ptp(X_norm,axis=0)}")
+
+
+w_norm, b_norm, hist = run_gradient_descent(X_norm, y_train, 1000, 1.0e-1, )
+
+#predict target using normalized features
+m = X_norm.shape[0]
+yp = np.zeros(m)
+for i in range(m):
+    yp[i] = np.dot(X_norm[i], w_norm) + b_norm
+
+    # Her özellik için gerçek fiyatlar (mavi) ve modelin tahmin ettiği fiyatlar (turuncu) yan yana görselleştirilir. 
+fig,ax=plt.subplots(1,4,figsize=(12, 3),sharey=True)
+for i in range(len(ax)):
+    ax[i].scatter(X_train[:,i],y_train, label = 'target')
+    ax[i].set_xlabel(X_features[i])
+    ax[i].scatter(X_train[:,i],yp,color=dlc["dlorange"], label = 'predict')
+ax[0].set_ylabel("Price"); ax[0].legend();
+fig.suptitle("target versus prediction using z-score normalized model")
+plt.show()
+
+
+plt_equal_scale(X_train, X_norm, y_train)
---
+++ C1/W2/2_FeatEng_PolyReg.py
@@ -0,0 +1,55 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from lab_utils_multi import zscore_normalize_features, run_gradient_descent_feng
+np.set_printoptions(precision=2)  # reduced display precision on numpy arrays
+
+
+
+# create target data
+# Shape: (4,) -> Sadece 4 elemanlı bir dizi. Satır/sütun kavramı yok.
+x = np.arange(0, 20, 1)
+y = 1 + x**2
+# Shape: (4, 1) -> 4 satır, 1 sütunlu bir MATRİS.
+X = x.reshape(-1, 1)
+
+model_w,model_b = run_gradient_descent_feng(X,y,iterations=1000, alpha = 1e-2)
+
+plt.scatter(x, y, marker='x', c='r', label="Actual Value"); plt.title("no feature engineering")
+plt.plot(x,X@model_w + model_b, label="Predicted Value");  plt.xlabel("X"); plt.ylabel("y"); plt.legend(); plt.show()
+
+
+X = x**2 
+X = X.reshape(-1, 1)
+
+model_w,model_b = run_gradient_descent_feng(X, y, iterations=10000, alpha = 1e-5)
+
+plt.scatter(x, y, marker='x', c='r', label="Actual Value"); plt.title("Added x**2 feature")
+plt.plot(x, np.dot(X,model_w) + model_b, label="Predicted Value"); plt.xlabel("x"); plt.ylabel("y"); plt.legend(); plt.show()
+print
+
+
+X = np.c_[x, x**2, x**3]
+
+model_w,model_b = run_gradient_descent_feng(X, y, iterations=10000, alpha=1e-7)
+
+plt.scatter(x, y, marker='x', c='r', label="Actual Value"); plt.title("x, x**2, x**3 features")
+plt.plot(x, X@model_w + model_b, label="Predicted Value"); plt.xlabel("x"); plt.ylabel("y"); plt.legend(); plt.show()
+
+X_features = ['x','x^2','x^3']
+
+#x^2 Dümdüz bir çizgi (Lineer) görünür! Veri x^2 uzayında lineerdir.
+# Bu, neden x^2 özelliğini kullandığımızda Lineer Regresyonun mükemmel çalıştığını kanıtlar
+fig,ax=plt.subplots(1, 3, figsize=(12, 3), sharey=True)
+for i in range(len(ax)):
+    ax[i].scatter(X[:,i],y)
+    ax[i].set_xlabel(X_features[i])
+ax[0].set_ylabel("y")
+plt.show()
+
+
+X = zscore_normalize_features(X) 
+
+model_w, model_b = run_gradient_descent_feng(X, y, iterations=100000, alpha=1e-1)
+
+plt.scatter(x, y, marker='x', c='r', label="Actual Value"); plt.title("Normalized x x**2, x**3 feature")
+plt.plot(x,X@model_w + model_b, label="Predicted Value"); plt.xlabel("x"); plt.ylabel("y"); plt.legend(); plt.show()
